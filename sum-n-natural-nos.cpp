a0ca6cc2230df457a777152c342f3aec264dec57
Vivek Dev Shah <vivek65.ugcs20@iiitranchi.ac.in>
2022-11-04 14:21:52 -0700

    Update sum-n-natural-nos.cpp

--- sum-n-natural-nos.cpp
+++ sum-n-natural-nos.cpp
@@ -9,6 +9,20 @@ int sum(int n){
         return n+sum(n-1);
     }
 }
+
+// int sum(int n){
+//     return ((n*(n+1))/2);
+// }
+
+// int sum(int n){
+//     int sum=0;
+//     n=n+1;
+//     while(n--){
+//         sum=sum+n;
+//     }
+//     return sum;
+// }
+
 int main(){
     cout<<sum(10);
 }
