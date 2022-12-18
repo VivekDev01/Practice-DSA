53381b9babdcba337cba6b63967c0f426bb8df46
Vivek Dev Shah <vivek65.ugcs20@iiitranchi.ac.in>
2022-12-18 02:26:31 -0800

    Create fibonacci-series-m0-using-loop.cpp

---
+++ fibonacci-series-m0-using-loop.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int fib(int n){
+  int t0=0, t1=1,s;
+  for(int i=2;i<=n;i++){
+      s=t0+t1;
+      t0=t1;
+      t1=s;
+  }
+  return s;
+}
+int main(){
+    cout<<fib(10);
+}
+
+//time complexity= O(n)
