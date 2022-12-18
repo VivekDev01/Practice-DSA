7ab635811972293a869821b8f2f27a63e47723cc
Vivek Dev Shah <vivek65.ugcs20@iiitranchi.ac.in>
2022-12-18 01:02:20 -0800

    Create tailor-series-method-3-using-horners-rule-using-recursion.cpp

---
+++ tailor-series-method-3-using-horners-rule-using-recursion.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+double e(int x, int n){
+    static double s=1;
+    if(n==0){
+        return s;
+    }
+    else{
+        s=1+(double(x)/double(n))*s;
+        return e(x, n-1);
+    }
+}
+
+int main(){
+    cout<<e(1,10);
+}
