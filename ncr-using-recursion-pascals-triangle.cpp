8ef12e9c6d7f3c9abd94a960b6c7b98ba91b98e4
Vivek Dev Shah <vivek65.ugcs20@iiitranchi.ac.in>
2022-12-18 22:28:40 -0800

    Create ncr-using-recursion-pascals-triangle.cpp

---
+++ ncr-using-recursion-pascals-triangle.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int nCr(int n, int r){
+    if(r==0 || n==r){
+        return 1;
+    }
+    else{
+        return nCr(n-1, r-1)+nCr(n-1,r); //using pascal's triangle
+    }
+}
+
+int main(){
+    cout<<nCr(6,3);
+}
